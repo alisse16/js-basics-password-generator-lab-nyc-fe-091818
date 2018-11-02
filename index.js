732b9d5c4f62e935cd33b6eca6cf1f32df50bd72
Alisse Frandsen <alisse.frandsen@gmail.com>
2018-11-02 00:18:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  //1. figure out pw length
+  
+  
+  
+  //2. make a password up 
+  
+  
+  
+  //3. keep track of given requirements 
+  
+  
+  
+  
 });
