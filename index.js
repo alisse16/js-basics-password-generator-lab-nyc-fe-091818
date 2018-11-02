834cdd6eb00348a8d672287896220c6a16d82a8e
Alisse Frandsen <alisse.frandsen@gmail.com>
2018-11-02 00:30:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
   //start your code here
   const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
   const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  const numbers = ''
+  const numbers = '0123456789'
   const specialCharacters = ''
   
   //1. figure out pw length
