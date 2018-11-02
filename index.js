7107c247164e09f63bc53a625a5644a26f9eef44
Alisse Frandsen <alisse.frandsen@gmail.com>
2018-11-02 00:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ window.addEventListener("load", () => {
   document.addEventListener('submit', (event) => {
     //don't refresh
     event.preventDefault()
+    
     const input = document.querySelector('input')
     const passwordLength = input.value 
     console.log(passwordLength)
+    let password = '' //start with empty password 
+    
+    // build the password up until passwordLength
+    for (let i = 0; i < passwordLength; i++) {
+      
+    }
   })
   
   
